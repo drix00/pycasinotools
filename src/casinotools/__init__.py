48332a80fcbe305c82e87ba564142eccede55214
hdemers <hdemers@lain>
2014-02-04 21:40:00 -0500

    Add initial files for mercurial, eclipse and testing.

---
+++ src/casinotools/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+.. py:currentmodule:: casinotools.__init__
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+Main casinotools package initialization module.
+"""
+
+# Script information for the file.
+__author__ = "Hendrix Demers (hendrix.demers@mail.mcgill.ca)"
+__version__ = ""
+__date__ = ""
+__copyright__ = "Copyright (c) 2014 Hendrix Demers"
+__license__ = ""
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+
+# Project modules
+
+# Globals and constants variables.
