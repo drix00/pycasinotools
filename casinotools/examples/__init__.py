7ab7a8ac758396b46ecc804ce26e7e576642db48
hendrix_demers <hendrix_demers@localhost>
2014-10-30 19:51:24 -0400

    Remove casino 3 features which are not ready.

--- casinotools/examples/__init__.py
+++
@@ -1,6 +0,0 @@
-# Script information for the file.
-__author__ = "Hendrix Demers (hendrix.demers@mail.mcgill.ca)"
-__version__ = ""
-__date__ = ""
-__copyright__ = "Copyright (c) 2014 Hendrix Demers"
-__license__ = ""
