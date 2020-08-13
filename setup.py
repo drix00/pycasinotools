d64f4ad0746e71ed39163b22fe659fb71184855a
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2020-08-12 20:02:02 -0400

    Add missing requirement for testing

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
     tests_require=['pytest', 'coverage', 'pytest-cov'],
     extras_require={
         'testing': ['pytest', 'coverage', 'pytest-cov'],
-        'develop': ['setuptools', 'Sphinx', 'sphinx-rtd-theme', 'coverage', 'pytest', 'pytest-cov']
+        'develop': ['setuptools', 'Sphinx', 'sphinx-rtd-theme', 'coverage', 'pytest', 'pytest-cov', "cattr"]
     },
 
     license="Apache Software License 2.0",
