08a327c330a55959e73a1c0d19573231ad85a1d5
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2022-07-01 13:44:53 -0400

    Start implementing tests for code organization

--- tests/test_casinotools.py
+++ tests/test_casinotools.py
@@ -25,6 +25,7 @@ Tests for the :py:mod:`casinotools` package.
 
 # Standard library modules.
 import os.path
+from pathlib import Path
 
 # Third party modules.
 
@@ -94,3 +95,54 @@ def test_file_path_energy_cylindrical_log_dat(file_path_energy_cylindrical_dat):
 
 def test_file_path_energy_spherical_log_dat(file_path_energy_spherical_dat):
     assert os.path.isfile(file_path_energy_spherical_dat)
+
+
+def test_tests_layout_matches_source():
+    # verify that this file is - itself - in tests/
+    this_files_path = Path(__file__)
+    tests_dir = this_files_path.parent
+    assert tests_dir.name == "tests"
+
+    # get a path to the j_park/ source directory
+    j_park_path = Path(tests_dir.parent, "casinotools")
+
+    # loop through all test_*.py files in tests/
+    # (and its subdirectories)
+    for test_file_path in tests_dir.glob("**/test_*.py"):
+        # skip this file: we don't expect there to be a
+        # corresponding source file for this layout enforcer
+        if test_file_path == this_files_path:
+            continue
+
+        # construct the expected source_file_path
+        source_rel_dir = test_file_path.relative_to(tests_dir).parent
+        source_name = test_file_path.name.split("test_", maxsplit=1)[1]
+        source_file_path = Path(j_park_path, source_rel_dir, source_name)
+        source_path = source_rel_dir.stem
+
+        error_msg = f"{test_file_path} found, but {source_file_path} missing."
+
+        assert source_file_path.is_file() or source_path == Path(source_name).stem
+
+
+def test_source_layout_matches_tests():
+    # verify that this file is - itself - in tests/
+    this_files_path = Path(__file__)
+    tests_dir = this_files_path.parent
+    assert tests_dir.name == "tests"
+
+    # get a path to the j_park/ source directory
+    source_dir = Path(tests_dir.parent, "casinotools")
+
+    # loop through all test_*.py files in tests/
+    # (and its subdirectories)
+    for file_path in source_dir.glob("**/*.py"):
+        # construct the expected source_file_path
+        source_rel_dir = file_path.relative_to(source_dir).parent
+        source_name = file_path.name.split("test_", maxsplit=1)[1]
+        source_file_path = Path(source_dir, source_rel_dir, source_name)
+        source_rel_path = source_rel_dir.stem
+
+        error_msg = f"{file_path} found, but {source_file_path} missing."
+
+        assert source_file_path.is_file() or source_rel_path == Path(source_name).stem
